4d536c4067c79c7fca1bad919ff34d52bff2141b
Evhenii Khartaniuk <co6alt@gmail.com>
2023-03-03 02:35:39 +0200

    hw 7

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,4 +1,52 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector(".gallery");
+
+galleryContainer.addEventListener("click", addOriginalImg);
+
+galleryContainer.insertAdjacentHTML(
+  "afterbegin",
+  createGalaryMarkup(galleryItems)
+);
+
+function createGalaryMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`;
+    })
+    .join("");
+}
+
+createGalaryMarkup(galleryItems);
+
+function addOriginalImg(events) {
+  events.preventDefault();
+  if (!events.target.classList.contains("gallery__image")) {
+    return;
+  }
+
+  const instance = basicLightbox.create(`
+    <img src="${events.target.dataset.source}" width="800" height="600">
+`);
+
+  instance.show();
+
+  galleryContainer.addEventListener("keydown", imgClose);
+
+  function imgClose(event) {
+    console.log(event.code);
+    if (event.code === "Escape") {
+      instance.close();
+    }
+  }
+}
